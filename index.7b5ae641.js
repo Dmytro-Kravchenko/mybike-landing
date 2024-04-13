c40038de0fa3e9e561573d8fc02c0af5dac6fb49
Dmytro Kravchenko <dmytro318@gmail.com>
2024-04-13 22:27:00 +0300

    make build

---
+++ index.7b5ae641.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".contact-form");window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),e.addEventListener("submit",function(t){t.preventDefault(),e.reset()});
+//# sourceMappingURL=index.7b5ae641.js.map
